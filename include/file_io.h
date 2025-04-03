915c7724f7d074eddc239abf1f132585fab9987c
zufichris <zufichris@gmail.com>
2025-04-03 23:21:31 +0400

    Updated file hearders

--- include/file_io.h
+++ include/file_io.h
@@ -1,10 +1,23 @@
 #ifndef FILE_IO_H
 #define FILE_IO_H
 
+typedef struct
+{
+    char **columns;
+    int columns_count;
+} CsvRow;
 
+typedef struct
+{
+    CsvRow **rows;
+    int rows_count;
+    char *delimiter;
+    char *filePath;
+    int capacity;
+} CsvData;
 
-void saveData(char *data);
-void exportData(char*data);
-void openFile(char *fileType,char* path);
+FILE *loadFile(const char *file_name);
+CsvRow *readRow(const char *file_name);
+void appendRow(CsvData *, CsvRow *);
 
 #endif
